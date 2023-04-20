d24cd1a5649baa3579bf0e9e153e59f97a2d3de4
DennisMatenge <mateshdennis@gmail.com>
2023-04-20 21:30:33 +0300

    fisrt commit

---
+++ index.js
@@ -0,0 +1,22 @@
+
+// Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+let num3 = 17;
+let num4 = 3;
+
+let num5 = 20; 
+let num6 = 8; 
+
+let mod = (num5 % num6 + num6) % num6;
+mod = (mod + num6 - 4) % num6; 
+
+console.log(mod); 
+
+
+
+let set = [5, 10, 15, 20, 7, 3];
+let max = Math.max(...set);
